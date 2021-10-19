e7d186a470de7ec323b9f5408fc621fec60b7003
JonathanPMoraes <88660067+jonathanlbt1@users.noreply.github.com>
2021-10-19 17:17:09 -0300

    Exercicio 8 e 10

---
+++ exercicio08.js
@@ -0,0 +1,8 @@
+// console.log("O console funcionou corretamente")
+
+var vel = 60.5
+console.log(`A velocidade de seu carro é ${vel}km por hora`)
+if (vel >= 60) {
+    console.log('Você ultrapassou a velocidade permitida. Você vai ser multado.')
+}
+console.log('Dirija sempre usando cinto de segurança')
